e0d423c096cfaf274ad1616797a459c478733acf
xuelingkang <574290057@qq.com>
2019-04-06 12:59:36 +0800

    重构目录结构

--- src/main/java/com/example/config/FilterConfig.java
+++ src/main/java/com/example/config/FilterConfig.java
@@ -4,6 +4,7 @@ import com.example.filter.SwaggerFilter;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import springfox.documentation.swagger2.web.Swagger2Controller;
 
 @Configuration
 public class FilterConfig {
@@ -17,7 +18,7 @@ public class FilterConfig {
     public FilterRegistrationBean<SwaggerFilter> swaggerFilterFilterRegistrationBean(SwaggerFilter swaggerFilter) {
         FilterRegistrationBean<SwaggerFilter> registration = new FilterRegistrationBean<>();
         registration.setFilter(swaggerFilter);
-        registration.addUrlPatterns("/v2/api-docs");
+        registration.addUrlPatterns(Swagger2Controller.DEFAULT_URL);
         registration.setName("swaggerFilter");
         return registration;
     }

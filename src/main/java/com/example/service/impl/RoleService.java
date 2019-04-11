34a1846ee491f1b3826da5b92a968a319c248360
xuelingkang <574290057@qq.com>
2019-04-11 14:44:03 +0800

    优化修改角色

--- src/main/java/com/example/service/impl/RoleService.java
+++ src/main/java/com/example/service/impl/RoleService.java
@@ -1,7 +1,6 @@
 package com.example.service.impl;
 
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
-import com.baomidou.mybatisplus.core.conditions.update.UpdateWrapper;
 import com.example.mapper.RoleMapper;
 import com.example.model.po.Resource;
 import com.example.model.po.Role;
@@ -13,7 +12,6 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.ArrayList;
-import java.util.Iterator;
 import java.util.List;
 
 @Service
@@ -50,35 +48,7 @@ public class RoleService extends BaseService<RoleMapper, Role> implements IRoleS
         for (Resource resource: resources) {
             links.add(new RoleResourceLink(roleId, resource.getId()));
         }
-        List<RoleResourceLink> oldLinks = roleResourceLinkService.list(new QueryWrapper<>(new RoleResourceLink().setRoleId(roleId)));
-        QueryWrapper<RoleResourceLink> queryWrapper = new QueryWrapper<>();
-        Iterator<RoleResourceLink> oldLinksIterator = oldLinks.iterator();
-        boolean flag = false;
-        a: while (oldLinksIterator.hasNext()) {
-            RoleResourceLink oldLink = oldLinksIterator.next();
-            Iterator<RoleResourceLink> linksIterator = links.iterator();
-            while (linksIterator.hasNext()) {
-                RoleResourceLink link = linksIterator.next();
-                if (oldLink.getResourceId().equals(link.getResourceId())) {
-                    UpdateWrapper<RoleResourceLink> updateWrapper = new UpdateWrapper<>();
-                    updateWrapper.eq("role_id", link.getRoleId());
-                    updateWrapper.eq("resource_id", link.getResourceId());
-                    roleResourceLinkService.update(link, updateWrapper);
-                    linksIterator.remove();
-                    continue a;
-                }
-            }
-            queryWrapper.eq("role_id", oldLink.getRoleId());
-            queryWrapper.eq("resource_id", oldLink.getResourceId());
-            queryWrapper.or();
-            flag = true;
-        }
-        if (flag) {
-            roleResourceLinkService.remove(queryWrapper);
-        }
-        if (links.size()>0) {
-            roleResourceLinkService.saveBatch(links);
-        }
+        roleResourceLinkService.merge(links, new QueryWrapper<>(new RoleResourceLink().setRoleId(roleId)));
     }
 
 }

c35976d47d039fea96f935234aa361fdaeeeee04
xuelingkang <574290057@qq.com>
2019-04-14 21:18:06 +0800

    删了几个单元测试

--- src/test/java/com/example/SpringBootDemoApplicationTests.java
+++ src/test/java/com/example/SpringBootDemoApplicationTests.java
@@ -19,51 +19,9 @@ import org.springframework.test.context.junit4.SpringRunner;
 public class SpringBootDemoApplicationTests {
 
     @Autowired
-    private RedisTemplate<String, Object> redisTemplate;
-    @Autowired
-    private UserMapper userMapper;
-    @Autowired
     private PasswordEncoder passwordEncoder;
     @Autowired
     private ApplicationContext context;
-
-    @Test
-    public void contextLoads() {
-        log.info("info");
-        log.debug("debug");
-        log.trace("trace");
-        log.warn("warn");
-        log.error("error");
-    }
-
-    @Test
-    public void testRedis() {
-        User user = userMapper.selectById(1);
-        redisTemplate.opsForValue().set("user", user);
-        Object obj = redisTemplate.opsForValue().get("user");
-        System.out.println(obj);
-        System.out.println(obj.getClass());
-    }
-
-    @Test
-    public void testArray() throws IllegalAccessException, InstantiationException {
-        User[] users = new User[7];
-        Object[] objects = (Object[]) users;
-        System.out.println(objects.length);
-        System.out.println(objects.getClass());
-    }
-
-    @Test
-    public void testQueryWrapper() {
-        QueryWrapper wrapper = new QueryWrapper();
-        wrapper.eq("user_id", 1);
-        wrapper.eq("role_id", 1);
-        wrapper.or();
-        wrapper.eq("user_id", 2);
-        wrapper.eq("role_id", 3);
-        wrapper.or();
-        System.out.println(wrapper.getCustomSqlSegment());
-    }
 
     @Test
     public void testPassword() {

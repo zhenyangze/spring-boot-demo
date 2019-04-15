4742809f5bc0cc1189e32627d28bf976756448bc
xuelingkang <574290057@qq.com>
2019-04-15 23:09:27 +0800

    统一方法命名

--- src/main/java/com/example/mapper/IBaseMapper.java
+++ src/main/java/com/example/mapper/IBaseMapper.java
@@ -2,7 +2,6 @@ package com.example.mapper;
 
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
-import com.example.exception.ProjectException;
 import com.example.model.BaseModel;
 import com.example.params.Params;
 import org.apache.ibatis.annotations.Param;
@@ -11,16 +10,10 @@ import java.util.List;
 
 public interface IBaseMapper<T extends BaseModel> extends BaseMapper<T> {
 
-    default List<T> customSelectPage(Page<T> page, @Param("params") Params<T> params) {
-        throw new ProjectException("方法未实现！");
-    }
+    List<T> customSelectPage(Page<T> page, @Param("params") Params<T> params);
 
-    default T customSelectById(Integer id) {
-        throw new ProjectException("方法未实现！");
-    }
+    T customSelectById(Integer id);
 
-    default T customSelectOne(@Param("params") Params<T> params) {
-        throw new ProjectException("方法未实现！");
-    }
+    T customSelectOne(@Param("params") Params<T> params);
 
 }

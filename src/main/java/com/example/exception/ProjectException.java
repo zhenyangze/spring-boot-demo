c7f8627320abed4243877507f06086be2567ac64
xuelingkang <574290057@qq.com>
2019-04-15 13:01:13 +0800

    修改构造函数

--- src/main/java/com/example/exception/ProjectException.java
+++ src/main/java/com/example/exception/ProjectException.java
@@ -5,23 +5,8 @@ package com.example.exception;
  */
 public class ProjectException extends RuntimeException {
 
-    public ProjectException() {
-    }
-
-    public ProjectException(String message) {
-        super(message);
-    }
-
     public ProjectException(String message, Throwable cause) {
         super(message, cause);
-    }
-
-    public ProjectException(Throwable cause) {
-        super(cause);
-    }
-
-    public ProjectException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
-        super(message, cause, enableSuppression, writableStackTrace);
     }
 
 }

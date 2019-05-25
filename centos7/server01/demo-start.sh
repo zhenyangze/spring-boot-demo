e6a2b13603363ad427e159282ae6b04b6c5568aa
xuelingkang <574290057@qq.com>
2019-05-25 21:48:48 +0800

    服务器配置文件

--- centos7/server01/demo-start.sh
+++
@@ -1,5 +0,0 @@
-#/bin/bash
-dir=/root/spring-boot-demo
-jar=${dir}/spring-boot-demo.jar
-log=${dir}/spring-boot-demo.log
-nohup java -Dfile.encoding=utf-8 -jar ${jar} --spring.mail.username=xuelingkang@163.com --spring.mail.password=163sqm > ${log} 2>&1 &

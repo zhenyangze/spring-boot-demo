41d1a3f22c5d63587871828576ac2763d5162303
xuelingkang <574290057@qq.com>
2019-06-05 00:06:33 +0800

    修改kafka启动脚本

--- centos7/server02/kafka-start.sh
+++ centos7/server02/kafka-start.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-sh /usr/local/kafka/bin/kafka-server-start.sh /usr/local/kafka/config/server.properties 1>/dev/null  2>&1 &
+sh /usr/local/kafka/bin/kafka-server-start.sh /usr/local/kafka/config/server.properties 1>/dev/null 2>&1 &
 echo "kafka启动完成"

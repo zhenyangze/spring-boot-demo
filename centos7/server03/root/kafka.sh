1832f8e7dcb4fb02ae1a561438a1e7d929ba3608
薛凌康 <574290057@qq.com>
2019-07-17 12:36:18 +0800

    增加了一个服务器

--- centos7/server03/root/kafka.sh
+++ centos7/server03/root/kafka.sh
@@ -2,10 +2,11 @@
 docker run -d --name kafka \
 --publish 9092:9092 \
 --link zookeeper \
---add-host server02:172.26.245.48 \
 --env KAFKA_ZOOKEEPER_CONNECT=zookeeper:2181 \
---env KAFKA_ADVERTISED_HOST_NAME=server02 \
+--env KAFKA_ADVERTISED_HOST_NAME=172.26.245.49 \
 --env KAFKA_ADVERTISED_PORT=9092 \
---env KAFKA_JVM_PERFORMANCE_OPTS=' -Xmx256m -Xms256m' \
+--env KAFKA_JVM_PERFORMANCE_OPTS=' -Xmx512m -Xms512m' \
 --volume /etc/localtime:/etc/localtime \
+--volume /etc/timezone:/etc/timezone \
 wurstmeister/kafka:2.11-2.0.1
+
--- centos7/server03/root/zookeeper.sh
+++ centos7/server03/root/zookeeper.sh
@@ -1,6 +1,8 @@
 #/bin/bash
 docker run -d --name zookeeper \
 -v /etc/localtime:/etc/localtime \
+-v /etc/timezone:/etc/timezone \
 -p 2181 \
 -t \
 wurstmeister/zookeeper
+

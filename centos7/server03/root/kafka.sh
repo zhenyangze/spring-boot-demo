d559282cb7739aa97879fb5ccc0747893b44c8d5
xuelingkang <574290057@qq.com>
2019-07-20 22:51:18 +0800

    修改docker容器启动命令

--- centos7/server03/root/kafka.sh
+++ centos7/server03/root/kafka.sh
@@ -1,13 +1,14 @@
 #/bin/bash
-docker run -d --name kafka \
+docker run  -d --name kafka \
 --restart=always \
---publish 9092:9092 \
---link zookeeper \
---env KAFKA_ZOOKEEPER_CONNECT=zookeeper:2181 \
---env KAFKA_ADVERTISED_HOST_NAME=172.26.245.49 \
---env KAFKA_ADVERTISED_PORT=9092 \
---env KAFKA_JVM_PERFORMANCE_OPTS=' -Xmx512m -Xms512m' \
---volume /etc/localtime:/etc/localtime \
---volume /etc/timezone:/etc/timezone \
+--add-host ali-server03:172.26.245.49 \
+-v /etc/localtime:/etc/localtime \
+-v /etc/timezone:/etc/timezone \
+-p 9092:9092 \
+-e KAFKA_BROKER_ID=0 \
+-e KAFKA_ZOOKEEPER_CONNECT=ali-server03:2181 \
+-e KAFKA_ADVERTISED_LISTENERS=PLAINTEXT://ali-server03:9092 \
+-e KAFKA_LISTENERS=PLAINTEXT://0.0.0.0:9092 \
+-e KAFKA_JVM_PERFORMANCE_OPTS=' -Xmx512m -Xms512m' \
+-t \
 wurstmeister/kafka:2.11-2.0.1
-
--- centos7/server03/root/zookeeper.sh
+++ centos7/server03/root/zookeeper.sh
@@ -3,7 +3,6 @@ docker run -d --name zookeeper \
 --restart=always \
 -v /etc/localtime:/etc/localtime \
 -v /etc/timezone:/etc/timezone \
--p 2181 \
+-p 2181:2181 \
 -t \
 wurstmeister/zookeeper
-

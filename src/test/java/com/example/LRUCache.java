ec006b4cc7e56754955a52d75b4617f37c02392d
xuelingkang <574290057@qq.com>
2019-07-31 23:37:20 +0800

    测试案例

---
+++ src/test/java/com/example/LRUCache.java
@@ -0,0 +1,17 @@
+package com.example;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+public class LRUCache<K, V> extends LinkedHashMap<K, V> {
+    private int size;
+    private static final float loadFactor = 0.75f;
+    public LRUCache(int size) {
+        super((int) Math.ceil(size/loadFactor) + 1, loadFactor, true);
+        this.size = size;
+    }
+    @Override
+    protected boolean removeEldestEntry(Map.Entry<K, V> eldest) {
+        return size()>size;
+    }
+}

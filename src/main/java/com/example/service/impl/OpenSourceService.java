2047dd726e5f47c3e30be918fde06f2c7a94e21e
薛凌康 <574290057@qq.com>
2019-08-15 16:25:09 +0800

    开源录入人和修改人

--- src/main/java/com/example/service/impl/OpenSourceService.java
+++ src/main/java/com/example/service/impl/OpenSourceService.java
@@ -5,6 +5,7 @@ import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.example.mapper.OpenSourceMapper;
 import com.example.model.po.OpenSource;
 import com.example.model.po.OpenSourceIntro;
+import com.example.model.po.User;
 import com.example.params.Params;
 import com.example.service.IOpenSourceIntroService;
 import com.example.service.IOpenSourceService;
@@ -31,6 +32,12 @@ public class OpenSourceService extends BaseService<OpenSourceMapper, OpenSource>
     @Override
     @Transactional
     public void customSave(OpenSource openSource) {
+        User currentUser = currentUser();
+        long now = System.currentTimeMillis();
+        openSource.setSaveUserId(currentUser.getId());
+        openSource.setSaveTime(now);
+        openSource.setUpdateUserId(currentUser.getId());
+        openSource.setUpdateTime(now);
         baseMapper.insert(openSource);
         Integer openSourceId = openSource.getId();
         OpenSourceIntro intro = openSource.getIntro();
@@ -41,6 +48,10 @@ public class OpenSourceService extends BaseService<OpenSourceMapper, OpenSource>
     @Override
     @Transactional
     public void customUpdateById(OpenSource openSource) {
+        User currentUser = currentUser();
+        long now = System.currentTimeMillis();
+        openSource.setUpdateUserId(currentUser.getId());
+        openSource.setUpdateTime(now);
         baseMapper.updateById(openSource);
         Integer openSourceId = openSource.getId();
         OpenSourceIntro intro = openSource.getIntro();

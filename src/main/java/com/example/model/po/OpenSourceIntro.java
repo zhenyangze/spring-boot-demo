6918a94d18249c3ee8e636527098e4f1beca4770
薛凌康 <574290057@qq.com>
2019-08-16 16:03:33 +0800

    增加参数校验

--- src/main/java/com/example/model/po/OpenSourceIntro.java
+++ src/main/java/com/example/model/po/OpenSourceIntro.java
@@ -8,6 +8,7 @@ import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
 
 import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Null;
 
 @Data
@@ -17,6 +18,7 @@ public class OpenSourceIntro extends BaseModel {
     private static final long serialVersionUID = 1L;
     @ApiModelProperty(value = "说明id")
     @Null(groups = {OpenSourceInsert.class}, message = "说明id必须为空")
+    @NotNull(groups = {OpenSourceUpdate.class}, message = "说明id不能为空")
     private Integer id;
     @ApiModelProperty(value = "说明内容")
     @NotBlank(groups = {OpenSourceInsert.class, OpenSourceUpdate.class}, message = "说明内容不能为空")

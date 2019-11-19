48e3975ed261c22ef25683ae0c5b1fb537905744
xuelingkang <574290057@qq.com>
2019-11-19 23:03:50 +0800

    生产环境redis改为单节点

--- centos7/server01/root/redis.sh
+++ centos7/server01/root/redis.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
-docker run -d --name redis6379 \
+docker run -d --name redis \
 --restart=always \
 -v /mnt/redis/6379:/data \
 -v /etc/localtime:/etc/localtime \
 -v /etc/timezone:/etc/timezone \
---network host \
+-p 6379:6379 \
 redis:5-alpine \
 redis-server --appendonly yes \
 --port 6379 \
---requirepass demo \
---masterauth demo
+--requirepass demo
